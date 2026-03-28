6d38ac1e4b8cc0821fb9a538cbff600b8de51efe
agatamunii-dev <agata.munii@gmail.com>
2026-03-28 16:05:06 +0200

    Create BoxingTester.cs

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,15 @@
+namespace Variant2;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;     
+        int c = (int)b;    
+
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+        Console.WriteLine($"c = {c}");
+    }
+}
