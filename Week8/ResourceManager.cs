0290ee52d2e7456c1d37ebe2a0ce71b97e62f6d4
agatamunii-dev <agata.munii@gmail.com>
2026-03-28 15:54:14 +0200

    Add ResourceManager class for managing resources

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+namespace Variant1;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var r in _resources)
+        {
+            r.Open();
+        }
+    }
+
+    public void CloseAll()
+    {
+        foreach (var r in _resources)
+        {
+            r.Close();
+        }
+    }
+}
